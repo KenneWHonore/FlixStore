84575edacc3ada56f7dd1ad830f19ee3d6afd91e
kenne <kennewhonore0@icloud.com>
2025-05-21 15:43:33 +0200

    Ajoutez des fichiers projet.

---
+++ FlixStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlixStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
